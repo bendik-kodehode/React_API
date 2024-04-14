f5d368f4e402637ded80e86d1348c2da318e84ae
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-04-14 21:59:59 +0200

    Working on linking for details page

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default function ListItem({item}) {
     return (
     <div>
-        <h2>{item.name}</h2>
+        <Link to="/">
+            <h2>{item.name}</h2>
+        </Link>
         <p>{item.year}</p>
         <p>{item.type}</p>
     </div>
