3d267b3f474c02dac64d033e4e2f7ddba3da462d
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-04-19 13:27:04 +0200

    Added search icon to search bar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,8 @@
 import { useQuery } from "../../contexts/QueryContext";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Searchbar() {
     const { query, setQuery } = useQuery();
@@ -12,20 +15,39 @@ export default function Searchbar() {
         navigate(`/results/${newQuery}`);
     }
     return (
-        <form onSubmit={handleInput}>
+        <form 
+            onSubmit={handleInput} 
+            style={{
+                position: "relative",
+            }}>
             <input 
                 name="inputField"
                 type="text"
                 style={{
-                    height: "25px",
+                    height: "1.8rem",
                     outline: "none",
                     border: "none",
-                    borderRadius: "10px",
-                    padding: "5px"
+                    borderRadius: "1rem",
+                    // padding: "5px",
+                    paddingRight: "1.5rem",
+                    padding: "0.25rem 2.2rem 0.25rem 0.5rem"
+
                 }}
                 required
                 autoComplete="off"
             />
+            <button type="submit" 
+                style={{
+                    border: "none", 
+                    background: "transparent",
+                    position: "absolute",
+                    right: "5%",
+                    top: "50%",
+                    transform: "translateY(-50%)",
+                    cursor: "pointer",
+                    }}>
+                <FontAwesomeIcon icon={faMagnifyingGlass} style={{fontSize: "1.2rem", color: "gray"}}/>
+            </button>
         </form>
     )
 }
