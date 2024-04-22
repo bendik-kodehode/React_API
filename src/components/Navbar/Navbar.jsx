e044c0a3be4ec0ce1ce3d289441ababa63d052a5
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-04-22 14:45:44 +0200

    Added classes on Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,27 @@
 import { NavLink } from "react-router-dom"
 import { useQuery } from "../../contexts/QueryContext"
-import "./Navbar.module.css"
+import styles from "./Navbar.module.css"
 
 export default function Navbar() {
     const { query } = useQuery();
 
     return (
-    <nav>
-        <NavLink to="/"><h1>Watchtower</h1></NavLink>
-        <ul>
-            <li>
-                <NavLink to="/"><h2>SEARCH</h2></NavLink>
+    <nav className={styles.navbar}>
+        <NavLink to="/" className={styles.anchor}><h1 className={styles.title}>Watchtower</h1></NavLink>
+        <ul className={styles.list}>
+            <li className={styles.listItem}>
+                <NavLink to="/" className={styles.anchor}>
+                    <h2 className={styles.subtitle}>SEARCH</h2>
+                </NavLink>
             </li>
-            <li>
+            <li className={styles.listItem}>
                 {
                     query ?
-                    <NavLink to={`/results/${query}`}>
-                        <h2>RESULTS</h2>
+                    <NavLink to={`/results/${query}`} className={styles.anchor}>
+                        <h2 className={styles.subtitle}>RESULTS</h2>
                     </NavLink> :
-                    <NavLink style={{opacity: "50%", cursor: "default"}}>
-                        <h2>RESULTS</h2>
+                    <NavLink style={{opacity: "50%", cursor: "default"}} className={styles.anchor}>
+                        <h2 className={styles.subtitle}>RESULTS</h2>
                     </NavLink>
                 }
             </li>
